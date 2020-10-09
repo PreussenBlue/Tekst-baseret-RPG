b122142f775b4b9d90a31976dbb318f2d8fd2abe
xandermand <xander01@live.dk>
2020-10-09 14:34:02 +0200

    Update combat.java

--- combat.java
+++ combat.java
@@ -2,7 +2,7 @@ import java.util.Random;
 public class combat {
 
 	//makes a function for later for combat
-	static void fight(int hp, int ac, int speed, int attack){
+	 public static int fight(int hp, int ac, int speed, int attack, String name){
 		java.util.Scanner
 		tastatur = new java.util.Scanner(System.in);
 		Random rand = new Random();
@@ -12,30 +12,49 @@ public class combat {
 		int temp_attack = 6;
 		int temp_ac = 16;
 		// delete later
-		while (hp>0 || temp_hp>0) {
+		while (hp>0 && temp_hp>0) {
 			//player moves first
 			if (temp_speed>speed) {
-				System.out.print("Do you want to attack? (1) Or rest for a turn? (2)");
+				System.out.print("Do you want to attack? (1)");
 				int valg = tastatur.nextInt();
 				switch (valg) {
 				case 1:
+					//player attacks
 					int bonus = rand.nextInt(20);
-					if (bonus + temp_attack>ac) {
-						hp = hp-4;
-					if (bonus+temp_attack<ac){
-						System.out.println("You miss your attack");
+					if (bonus + temp_attack>=ac) {
+						int damage = rand.nextInt(7)+1;
+						hp = hp - damage;
+						System.out.println("You hit the " + name + " and deal " + damage + " damage to it \n");
+						System.out.println("enemy hp: " + hp);
+						
+					}
+					//you miss your attack
+					else if (bonus+temp_attack<ac){
+						System.out.println("You miss your attack \n");
+					}
+					//error
+					else {
+						System.out.println("Something went wrong\n");
 					}
 					break;
 					}
-				case 2:
-					temp_hp = temp_hp + 2;
-					break;
+				// monster attacks
+				int bonus = rand.nextInt(20);
+				if (attack + bonus>temp_ac) {
+					System.out.println("You are attacked by the " + name + " and it hits \n");
+					temp_hp = temp_hp - (attack/2);
+					System.out.println("Your hp is now: " + temp_hp + "\n");
 				}
-			int bonus = rand.nextInt(20);
-			if (attack + bonus>)
+				else if (attack + bonus<temp_ac) {
+					System.out.println("The " + name + " misses you!");
+				}
+				}
+				}
+			return temp_hp;
 			}
-		}
-	}
+		
+		
+	
 
 	public static void main(String[] args) {
 		Random rand = new Random();
@@ -49,16 +68,17 @@ public class combat {
 			int hp;
 			int attack;
 			int ac;
+			String name;
 		}
 		
 		System.out.println("As you enter the dank room, You Feel an eerie presence.");
-		int enemy_type = 3;
+		int enemy_type = rand.nextInt(3);
 		
 		switch (enemy_type) {
-		case 3:
+		case 0:
 			//for the goblin encounter
 			enemy goblin = new enemy();
-			goblin.speed = 5;
+			goblin.speed = 7;
 			System.out.println(goblin.speed);
 			goblin.hp = 10;
 			System.out.println(goblin.hp);
@@ -66,13 +86,59 @@ public class combat {
 			System.out.println(goblin.attack);
 			goblin.ac = 12;
 			System.out.println(goblin.ac);
-			
+			goblin.name = "Goblin";
+			System.out.println(goblin.name);
+			fight(goblin.hp, goblin.ac, goblin.speed, goblin.attack, goblin.name);
 			break;
 		
 		case 1:
-			//for the or
+			//for the orc encounter
+			enemy orc = new enemy();
+			orc.speed = 4;
+			System.out.println(orc.speed);
+			orc.hp = 14;
+			System.out.println(orc.hp);
+			orc.attack = 6;
+			System.out.println(orc.attack);
+			orc.ac = 14;
+			System.out.println(orc.ac);
+			orc.name = "Orc";
+			System.out.println(orc.name);
+			fight(orc.hp, orc.ac, orc.speed, orc.attack, orc.name);
+			break;
+			
+		case 2:
+			//for the mimic encounter
+			enemy mimic = new enemy();
+			mimic.speed = 2;
+			System.out.println(mimic.speed);
+			mimic.hp = 16;
+			System.out.println(mimic.hp);
+			mimic.attack = 5;
+			System.out.println(mimic.attack);
+			mimic.ac = 15;
+			System.out.println(mimic.ac);
+			mimic.name = "Mimic";
+			System.out.println(mimic.name);
+			fight(mimic.hp, mimic.ac, mimic.speed, mimic.attack, mimic.name);
+			break;
+			
+		case 3:
+			//for the hobgoblin encounter
+			enemy hgoblin = new enemy();
+			hgoblin.speed = 6;
+			System.out.println("enemy speed: " + hgoblin.speed);
+			hgoblin.hp = 16;
+			System.out.println("enemy hp: " + hgoblin.hp);
+			hgoblin.attack = 4;
+			System.out.println("enemy attack: " + hgoblin.attack);
+			hgoblin.ac = 14;
+			System.out.println("Enemy ac: " + hgoblin.ac);
+			hgoblin.name = "Hobgoblin";
+			System.out.println(hgoblin.name);
+			fight(hgoblin.hp, hgoblin.ac, hgoblin.speed, hgoblin.attack, hgoblin.name);
+			break;
 		}
-		
 	}
 
 }
